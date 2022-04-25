a86321e0d0fcddac22afe245597223ab09626929
SamanjaCartagena <cart.samanja09@gmail.com>
2022-04-24 21:29:43 -0400

    destination routers added

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,9 +1,27 @@
 <template>
-  <h1>Home Page</h1>
+<div>
+  <h1>All Destinations</h1>
+  <div class="destinations">
+  </div>
+  <router-link 
+  v-for="destination in destinations" 
+  :key="destination.id"
+  :to="destination.slug"
+  
+  ><h2>{{destination.name}}</h2>
+  <img :src="`/images/${destination.image}`"
+   :alt="destination.name">  </router-link>
+</div>
 </template>
 
 <script>
+import sourceData from '/data.json'
 export default {
+    data(){
+        return {
+            destinations:sourceData.destinations
+        }
+    }
 
 }
 </script>
